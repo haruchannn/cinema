443119d0b187d246ee9b97206f91c20641371186
haruchannn <haruka.senzaki@gmail.com>
2023-06-27 15:40:53 +0100

    lesson 4, 5;

--- database/migrations/2023_06_27_230111_create_practices_table.php
+++ database/migrations/2023_06_27_230111_create_practices_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePracticesTable3 extends Migration
+class CreatePracticesTable extends Migration
 {
     /**
      * Run the migrations.
